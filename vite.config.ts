0d86e25139403bbd6ea9908deabcde5b83d5e734
musho777 <129147695+musho777@users.noreply.github.com>
2025-05-15 22:12:04 +0400

    bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
+    host: '0.0.0.0',    
+    port: 5174,    
     proxy: {
       '/api': {
         target: 'https://xn----nbck7b7ald8atlv.xn--y9a3aq/halal.loc/public/api',
